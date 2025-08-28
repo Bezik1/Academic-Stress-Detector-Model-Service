c656660ff161ed3f12e0c1e24e6a08a9e9eed0d7
Mateusz <mateusz.adamowicz.lo1@gmail.com>
2025-08-28 20:13:49 +0200

    Implementing Dataset

---
+++ utils/dataset.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import torch
+from torch.utils.data import DataLoader, TensorDataset, random_split
+from sklearn.preprocessing import StandardScaler
+
+df = pd.read_csv("./data/StressLevelDataset.csv")
+
+X = df.drop("stress_level", axis=1).values
+Y = df["stress_level"].values 
+
+scaler = StandardScaler()
+X = scaler.fit_transform(X)
+
+X = torch.tensor(X, dtype=torch.float32)
+Y = torch.tensor(Y, dtype=torch.long)
+
+dataset = TensorDataset(X, Y)
+
+train_size = int(0.7 * len(dataset))
+val_size = int(0.1 * len(dataset))
+test_size = len(dataset) - train_size - val_size
+train_ds, val_ds, test_ds = random_split(dataset, [train_size, val_size, test_size])
+
+train_loader = DataLoader(train_ds, batch_size=32, shuffle=True)
+val_loader = DataLoader(val_ds, batch_size=32)
+test_loader = DataLoader(test_ds, batch_size=32)
+
+if __name__ == "__main__":
+    print(X)
+    print(X.shape)
+    
+    print(Y.shape)
